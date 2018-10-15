96e686ccd83505cc911cf394fb7426af9b22c219
Your Name <1504184877@qq.com>
2018-10-15 14:50:44 +0000

    setup

--- p3.c
+++ p3.c
@@ -1,4 +1,5 @@
 //problem3
+int main()
 int solve(double a, double, b, double c, double *root1, double *root2){
 double discriminent = 0;
  discriminant = b*b-4*a*c;
@@ -17,5 +18,11 @@ printf("%d roots",solve(a,b,c,1.0,1.0))
 
 }
 
+tester(1.0, 0.0, 4.0, "no solution");
+tester(1.0, 0.0, -4.0, "two solutions");
+tester(1.0, 0.0, 0.0, "one solution"); 
 
 
+
+return 0;
+}
