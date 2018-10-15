bc3b5c27447d9999c07627c53bc5527c51c67a6a
syy1997 <42967670+syy1997@users.noreply.github.com>
2018-10-15 11:25:37 -0400

    Update p3.c

--- p3.c
+++ p3.c
@@ -1,10 +1,14 @@
-//problem3
-int main()
-int solve(double a, double, b, double c, double *root1, double *root2){
-double discriminent = 0;
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+int solve(double a, double b, double c, double *root1, double *root2){
+double discriminant = 0;
  discriminant = b*b-4*a*c;
- root1 = (-b+sqrt(discriminant))/(2*a);
- root2 = (-b-sqrt(discriminant))/(2*a);
+ 
+ double r1 = (-b+sqrt(discriminant))/(2*a);
+ double r2 = (-b-sqrt(discriminant))/(2*a);
 
 if(discriminant > 0){return 2;}
 if(discriminant = 0){return 1;}
@@ -13,11 +17,10 @@ if(discriminant < 0){return 0;}
 
 }
 void tester(double a, double b, double c, char *msg){
-printf("%d roots",solve(a,b,c,1.0,1.0))
+printf("%d roots",solve(a,b,c,&a,&b));
 
 
 }
-
 tester(1.0, 0.0, 4.0, "no solution");
 tester(1.0, 0.0, -4.0, "two solutions");
 tester(1.0, 0.0, 0.0, "one solution"); 
